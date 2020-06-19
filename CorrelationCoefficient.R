0497bd9e3d046f3aaa2effce45c2648a90cd2908
emascillaro <36569453+emascillaro@users.noreply.github.com>
2020-06-19 17:09:25 -0400

    Update CorrelationCoefficient.R

--- CorrelationCoefficient.R
+++ CorrelationCoefficient.R
@@ -3,9 +3,9 @@ StateDeathRate <- (StateDeaths/StateCases)
 cor(StateDeathRate, StateHospitals)
 par(mar=c(1,1,1,1))
 plot(StateDeathRate, StateHospitals, 
-     main = "Relationship between Death Rate and Number of Hospitals by State",
-     xlab = "State Death Rate", ylab = "State Hospitals")
-abline(lm(StateDeathRate~StateHospitals))
+     main = "Relationship between Death Rate and # of Hospitals by State",
+     xlab = "State Death Rate", ylab = "State Hospitals", col = "coral")
+abline(lm(StateHospitals~StateDeathRate), col = "Red")
 lm(StateDeathRate~StateHospitals)
 
 #----------------------------------------------------------
@@ -15,7 +15,8 @@ cor(RegionDeathRate, RegionHospitals)
 par(mar=c(3,3,3,3))
 plot(RegionDeathRate, RegionHospitals, 
      main = "Relationship between Death Rate and Number of Hospitals by Region",
-     xlab = "Regional Death Rate", ylab = "Region Hospitals")
-abline(lm(RegionDeathRate~RegionHospitals))
+     xlab = "Regional Death Rate", ylab = "Region Hospitals",
+     col = "darkblue")
+abline(lm(RegionHospitals~RegionDeathRate), col = "purple")
 lm(RegionDeathRate~RegionHospitals)
 
